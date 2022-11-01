fcbc4d50111c271c33f560efde774b13ddab6020
js3g20 <js3g20@soton.ac.uk>
2022-11-01 18:42:47 +0000

    Create Variance / SD Test

---
+++ Multiplication/StochasticTest/Basic.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Neuron import *
+
+
+N = 512
+
+X = 50
+
+# calculation results
+z = []
+
+a = np.zeros(N, dtype=np.int32)
+b = np.zeros(N, dtype=np.int32)
+x = np.zeros(N, dtype=np.int32)
+
+var = 0
+
+# itterate through x y
+for i in range(1, X):
+    i = i / X
+    for j in range(1, X):
+        j = j / X
+
+        a = bitstream_generator_exact(i, N)
+        b = bitstream_generator_exact(j, N)
+        c = (a == 1) & (b == 1)
+        #c = (a == 1) | (b == 1)
+        c = bitstream_integrator(c)
+
+        y = i * j
+
+        var += (c - y) ** 2
+
+        z.append(((c - y) / y) * 100)
+        
+z = np.array(z)
+
+
+var = (var / z.size)
+
+thresholds = [5, 10]
+
+
+
+print("Stochastic Multiplication Test")
+print("Bit Depth: {}".format(N))
+print("Data Points: {}".format(z.size))
+print("Variance: {}".format(var))
+print("Standard Deviation: {}".format(var ** 0.5))
+
+for t in thresholds:
+    greater_than = z[z > t]
+    less_than = z[z < (-1 * t)]
+
+    print("Data Within {}%: {}".format(t, ((z.size - (greater_than.size + less_than.size)) / z.size) * 100))
+    print("Data Above {}%: {}".format(t, (greater_than.size / z.size) * 100))
+    print("Data Below {}%: {}".format(t, (less_than.size / z.size) * 100))
+
+
+plt.hist(z, bins=20)
+#plt.show()
