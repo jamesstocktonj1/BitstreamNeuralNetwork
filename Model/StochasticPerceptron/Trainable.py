5d2afe5ed2d772ac0747c7540f410b7dd6b94c4e
js3g20 <js3g20@soton.ac.uk>
2022-11-08 11:30:26 +0000

    Fix Training / Add Epochs

--- Model/StochasticPerceptron/Trainable.py
+++ Model/StochasticPerceptron/Trainable.py
@@ -11,7 +11,7 @@ trainingRate = 0.1
 
 # create dataset
 x = np.vstack([np.random.randint(0, 20, size=(X//2, 2))*0.025, np.random.randint(20, 40, size=(X//2, 2))*0.025])
-y = np.hstack([np.zeros(X//2), np.ones(X//2)]).reshape(-1, 1)
+y = np.hstack([np.ones(X//2) * -1, np.ones(X//2)]).reshape(-1, 1)
 
 
 # plot points
@@ -25,54 +25,69 @@ def plot_data(x, y, N):
     plt.close()
 
 
-def training_loop(x, y, r, N):
+def training_loop(x, y, r, N, epochs):
     # setup neuron with initial weights
     neuron = Neuron(2, N, N)
-    w = np.random.randint(0, 20, size=(2)) * 0.05
-    w = np.array([0.5, 0.5])
-    neuron.set_weights(w)
+    #w = np.random.randint(0, 20, size=(2)) * 0.05
+    weights = np.array([0.5, 0.5])
+    neuron.set_weights(weights)
 
-    correct = -np.ones(y.shape)
-    i = 0
+    for e in range(epochs):
+        correct = np.zeros(y.shape)
+        i = 0
 
-    # initial correct points
+        neuron.set_weights(weights)
+
+        # initial correct points
+        for rxy in range(len(x)):
+            x_hat0 = bitstream_generator_exact(x[rxy][0], N)
+            x_hat1 = bitstream_generator_exact(x[rxy][1], N)
+            y_hat = neuron.call(np.array([x_hat0, x_hat1]))
+            y_hat = bitstream_integrator(y_hat)
+
+            if (y_hat > 0.5) == (y[rxy] == 1):
+                correct[rxy] = 1
+
+        print("Pass: {}\tIncorrect Points: {}".format(e, np.sum(correct == 0)))
+
+        # train incorrect points
+        while np.sum(correct) < len(y):
+            rxy = np.random.choice(np.where(correct < 1)[0])
+            i += 1
+
+            weights = weights + r * (y[rxy] * x[rxy])
+            weights = np.clip(weights, 0, 1.0)
+            neuron.set_weights(weights)
+
+            #print(weights)
+
+            # run through neuron
+            x_hat0 = bitstream_generator_exact(x[rxy][0], N)
+            x_hat1 = bitstream_generator_exact(x[rxy][1], N)
+            y_hat = neuron.call(np.array([x_hat0, x_hat1]))
+            y_hat = bitstream_integrator(y_hat)
+
+            
+            # if correctly classified
+            if (y_hat > 0.5) == (y[rxy] == 1):
+                correct[rxy] = 1
+
+            # leave early
+            if i > (X * 10):
+                break
+
+    # evaluate model
+    correct = np.zeros(y.shape)
     for rxy in range(len(x)):
         x_hat0 = bitstream_generator_exact(x[rxy][0], N)
         x_hat1 = bitstream_generator_exact(x[rxy][1], N)
         y_hat = neuron.call(np.array([x_hat0, x_hat1]))
         y_hat = bitstream_integrator(y_hat)
 
-        if (y_hat > 0.5) == y[rxy]:
+        if (y_hat > 0.5) == (y[rxy] == 1):
             correct[rxy] = 1
 
-    weights = np.array([0.1, 0.1])
-
-    # train incorrect points
-    while np.sum(correct) < len(y):
-        rxy = np.random.choice(np.where(correct < 1)[0])
-        i += 1
-
-        weights = weights + r * (y[rxy] * x[rxy])
-        weights = np.clip(weights, 0, 1.0)
-        neuron.set_weights(weights)
-
-        print(weights)
-        
-
-        # run through neuron
-        x_hat0 = bitstream_generator_exact(x[rxy][0], N)
-        x_hat1 = bitstream_generator_exact(x[rxy][1], N)
-        y_hat = neuron.call(np.array([x_hat0, x_hat1]))
-        y_hat = bitstream_integrator(y_hat)
-
-        
-        # if correctly classified
-        if (y_hat > 0.5) == y[rxy]:
-            correct[rxy] = 1
-
-        # leave early
-        if i > X:
-            break
+    print("Model Evaluation, Incorrect Points: {}".format(np.sum(correct == 0)))
 
     return weights, neuron
 
@@ -102,7 +117,7 @@ def perceptron_flood(R, N, neuron):
     plt.close()
 
 
-def perceptron_dual_plot(x, y, R, N, neuron):
+def perceptron_dual_plot(x, y, R, N, neuron, weights):
     plt.figure()
     fig1 = plt.subplot(1, 2, 1)
     fig1.set_aspect("equal", adjustable="box")
@@ -130,19 +145,20 @@ def perceptron_dual_plot(x, y, R, N, neuron):
                 fig2.plot(i, j, 'ro')
             else:
                 fig2.plot(i, j, 'bo')
-
-
+    
+    fig1.set_title("Weights: {:.4f}, {:.4f}".format(weights[0], weights[1]))
+    plt.suptitle("Trained Perceptron {}-Bits".format(N))
     plt.savefig("images/perceptron_dual_{}bits.png".format(N))
     plt.close()
 
 
 for n in N:
 
-    w, neuron = training_loop(x, y, trainingRate, n)
     print("\nPerceptron {}-Bits".format(n))
+    w, neuron = training_loop(x, y, trainingRate, n, 5)
     print("Weights: {}".format(w))
 
-    perceptron_dual_plot(x, y, 50, n, neuron)
+    perceptron_dual_plot(x, y, 50, n, neuron, w)
 
     #plot_data(x, y, n)
     #perceptron_flood(50, n, neuron)
