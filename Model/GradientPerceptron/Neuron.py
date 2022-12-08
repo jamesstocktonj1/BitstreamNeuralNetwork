3757cbb1e748eee2fd58f5608f3d830b466d0e3e
js3g20 <js3g20@soton.ac.uk>
2022-12-08 16:30:04 +0000

    Add Gradient Calculation

--- Model/GradientPerceptron/Neuron.py
+++ Model/GradientPerceptron/Neuron.py
@@ -14,5 +14,13 @@ class Neuron:
         mult = x * self.weights
         return 1 - np.product(1 - mult)
 
+    def diff(self, y_hat, x):
+        w = self.weights
+
+        dw0 = -2 * x[0] * (1 - (x[1]*w[1])) * (y_hat - 1 + ((1 - (w[0]*x[0])) * (1 - (w[1]*x[1]))))
+        dw1 = -2 * x[1] * (1 - (x[0]*w[0])) * (y_hat - 1 + ((1 - (w[0]*x[0])) * (1 - (w[1]*x[1]))))
+
+        return np.array([dw0[0], dw1[0]])
+
     def decision(self, x):
         return (0.5 - (x * self.weights[0])) / (self.weights[1] - (x * self.weights[0] * self.weights[1]))
