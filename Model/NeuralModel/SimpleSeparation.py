e76faf9949b9792697cba850f06d384900af6075
js3g20 <js3g20@soton.ac.uk>
2022-12-09 23:35:17 +0000

    Improve Training

--- Model/NeuralModel/SimpleSeparation.py
+++ Model/NeuralModel/SimpleSeparation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 from NeuralModel import SimpleModel
 
@@ -25,56 +26,65 @@ x = np.interp(x, (x.min(), x.max()), (0, 1))
 y = np.hstack([np.zeros(X//2), np.ones(X//2)]).reshape(-1, 1)
 
 
+def plot_confusion_matrix(y1Data, y2Data):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    confMat = confusion_matrix(y1Data, y2Data)
+
+    ax.matshow(confMat, cmap=plt.cm.Greens, alpha=0.3)
+    for i in range(confMat.shape[0]):
+        for j in range(confMat.shape[1]):
+            plt.text(x=j, y=i, s=confMat[i, j], va='center', ha='center')
+
+    plt.xlabel("True Prediction")
+    plt.ylabel("Model Prediction")
+
+    plt.savefig("images/simple_separation_confused.png")
+    plt.close()
+
 
 def training_loop(x, y):
     model = SimpleModel(0.000125)
-
-    # model.layer1.weights = np.random.randn(2, 2)
-    # model.layer1.weights = np.interp(model.layer1.weights, (model.layer1.weights.min(), model.layer1.weights.max()), (0.4, 0.6))
-
-    # model.layer2.weights = np.random.randn(1,2)
-    # model.layer2.weights = np.interp(model.layer2.weights, (model.layer2.weights.min(), model.layer2.weights.max()), (0.4, 0.6))
-
-    # correct = np.zeros(y.shape)
     model.init_weights()
-    
-    # for e in range(2):
 
-    #     # initial correct points
-    #     for rxy in range(len(x)):
-    #         y_hat = model.call(x[rxy])
-
-    #         if (y_hat > 0.5) == (y[rxy] == 1):
-    #             correct[rxy] = 1
-
-    #     print("Pass: {}\tIncorrect Points: {}".format(e, np.sum(correct == 0)))
-
-    #     while np.sum(correct) < len(y):
-    #         rxy = np.random.choice(np.where(correct < 1)[0])
-            
-    #         grad1, grad2 = model.grad(x[rxy], y[rxy])
-    #         print(grad1, grad2)
-
-    #         # run through neuron
-    #         y_hat = model.call(x[rxy])
-
-    #         # if correctly classified
-    #         if (y_hat > 0.5) == (y[rxy] == 1):
-    #             correct[rxy] = 1
+    # set initial weights
+    for rxy in range(len(x)):
+        model.grad(x[rxy], y[rxy])
 
     for e in range(10):
+
+        # initial pass
+        correct = np.zeros(y.shape)
         for rxy in range(len(x)):
+            y_hat = model.call(x[rxy])
+
+            if (y_hat > 0.5) == (y[rxy] == 1):
+                correct[rxy] = 1
+
+        # train incorrect points
+        for rxy in np.where(correct < 1)[0]:
             grad1, grad2 = model.grad(x[rxy], y[rxy])
-            print(model.layer1.weights)
-            print(model.layer2.weights)
+
+        correct = np.zeros(y.shape)
+        for rxy in range(len(x)):
+            y_hat = model.call(x[rxy])
+
+            if (y_hat > 0.5) == (y[rxy] == 1):
+                correct[rxy] = 1
+
+        print("Epoch {}: {}/{}".format(e, correct.sum(), len(correct)))
     
     # evaluate model
     correct = np.zeros(y.shape)
+    y_hat = np.zeros(y.shape)
     for rxy in range(len(x)):
-        y_hat = model.call(x[rxy])
+        y_hat[rxy] = model.call(x[rxy])
 
-        if (y_hat > 0.5) == (y[rxy] == 1):
+        if (y_hat[rxy] > 0.5) == (y[rxy] == 1):
             correct[rxy] = 1
+
+    plot_confusion_matrix(y_hat > 0.5, y)
 
     print("Model Evaluation, Accuracy: {:.2f}%".format((np.sum(correct == 1) / len(correct)) * 100))
 
