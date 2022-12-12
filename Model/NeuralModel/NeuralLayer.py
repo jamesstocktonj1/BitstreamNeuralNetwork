Fix Gradient Descent

Model/NeuralModel/NeuralLayer.py
@@ -11,19 +11,16 @@ class NeuralLayer:
         self.input_size = input_size
         self.output_size = output_size
 
-    
+    def init_weights(self, u):
+        self.weights = 0.5 + u * np.random.randn(self.output_size, self.input_size)
+
+
     def grad_loss(self, x, y):
         grads = np.zeros((self.output_size, self.input_size))
 
         for i in range(self.output_size):
             for j in range(self.input_size):
-
-                grads[i,j] = -2 * x[j]
-                for k in range(self.input_size):
-                    if k != j:
-                        grads[i,j] *= 1 - (self.weights[i,k] * x[k])
-                
-                grads[i,j] *= y[i] - 1 + np.product(1 - (self.weights[i] * x))
+                grads[i,j] = y[i] - 1 + np.product(1 - (self.weights[i] * x))
         
         return grads
 
@@ -80,7 +77,7 @@ def neuron_layer_test():
 
     layer.weights = w
 
-    grads = layer.grad(a)
+    grads = layer.grad_weight(a)
 
     exp_grads = np.zeros((2,2))
     exp_grads[0,0] = -1 * a[0] * (1 - (w[0,1] * a[1]))
@@ -91,13 +88,13 @@ def neuron_layer_test():
     print("Expected: ", exp_grads)
     print("Got:      ", grads)
 
-    grads = layer.loss_grad(a, b)
+    grads = layer.grad_loss(a, b)
 
     exp_grads = np.zeros((2,2))
-    exp_grads[0,0] = -2 * a[0] * (1 - (w[0,1] * a[1])) * (b[0] - 1 + ((1 - (w[0,0] * a[0])) * (1 - (w[0,1] * a[1]))))
-    exp_grads[0,1] = -2 * a[1] * (1 - (w[0,0] * a[0])) * (b[0] - 1 + ((1 - (w[0,0] * a[0])) * (1 - (w[0,1] * a[1]))))
-    exp_grads[1,0] = -2 * a[0] * (1 - (w[1,1] * a[1])) * (b[1] - 1 + ((1 - (w[1,0] * a[0])) * (1 - (w[1,1] * a[1]))))
-    exp_grads[1,1] = -2 * a[1] * (1 - (w[1,0] * a[0])) * (b[1] - 1 + ((1 - (w[1,0] * a[0])) * (1 - (w[1,1] * a[1]))))
+    exp_grads[0,0] = -2 * w[0,0] * (1 - (w[0,1] * a[1])) * (b[0] - 1 + ((1 - (w[0,0] * a[0])) * (1 - (w[0,1] * a[1]))))
+    exp_grads[0,1] = -2 * w[0,1] * (1 - (w[0,0] * a[0])) * (b[0] - 1 + ((1 - (w[0,0] * a[0])) * (1 - (w[0,1] * a[1]))))
+    exp_grads[1,0] = -2 * w[1,0] * (1 - (w[1,1] * a[1])) * (b[1] - 1 + ((1 - (w[1,0] * a[0])) * (1 - (w[1,1] * a[1]))))
+    exp_grads[1,1] = -2 * w[1,1] * (1 - (w[1,0] * a[0])) * (b[1] - 1 + ((1 - (w[1,0] * a[0])) * (1 - (w[1,1] * a[1]))))
 
     print("Expected: ", exp_grads)
     print("Got:      ", grads)

Model/NeuralModel/SimpleSeparation.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from sklearn.metrics import confusion_matrix
 
-from NeuralModel import SimpleModel, BigModel
+from NeuralModel import SimpleModel
 
 
 X = 250
@@ -44,9 +45,50 @@ def plot_confusion_matrix(y1Data, y2Data):
     plt.close()
 
 
+def plot_decision(model):
+    plt.figure()
+
+    for i in range(50):
+        for j in range(50):
+            x = np.array([i/50, j/50])
+            y = model.call(x)
+
+            if y > 0.5:
+                plt.plot(i, j, '-ro')
+            else:
+                plt.plot(i, j, '-bx')
+
+    plt.savefig("images/simple_decision.png")
+    plt.close()
+
+def plot_3d_plane(model):
+
+    fig = plt.figure()
+    fig1 = fig.add_subplot(projection='3d')
+
+    x = np.arange(0, 1, 1/X)
+    y = np.arange(0, 1, 1/X)
+    x_t, y_t = np.meshgrid(x, y)
+
+    # neuronPlane = call_neuron(x_t, y_t, n)
+    neuronPlane = np.zeros((X, X))
+
+    for i in range(X):
+        for j in range(X):
+            neuronPlane[i,j] = model.call(np.array([x_t[i,j], y_t[i,j]]))
+
+    surf = fig1.plot_surface(x_t, y_t, neuronPlane, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # surf = fig1.plot_surface(x_t, y_t, neuronPlane, cmap=cm.coolwarm, rstride=1, cstride=1, alpha=None, antialiased=True)
+
+    fig.colorbar(surf)
+    plt.show()
+
 def training_loop(x, y):
-    model = SimpleModel(1)
-    model.init_weights()
+    model = SimpleModel(0.0025)
+
+    print(model.layer1.weights)
+    print(model.layer2.weights)
+    # model.init_weights()
 
     # set initial weights
     for rxy in range(len(x)):
@@ -59,20 +101,25 @@ def training_loop(x, y):
         for rxy in range(len(x)):
             y_hat = model.call(x[rxy])
 
-            correct[rxy] = ((y_hat > 0.5) == (y[rxy] == 1)) * 1
+            correct[rxy] = ((y_hat[0] > 0.5) == (y[rxy] == 1)) * 1
 
         # train incorrect points
         for rxy in np.where(correct < 1)[0]:
             grads = model.grad(x[rxy], y[rxy])
+            # print(grads)
 
         correct = np.zeros(y.shape)
+        modelLoss = 0
         for rxy in range(len(x)):
             y_hat = model.call(x[rxy])
 
-            if (y_hat > 0.5) == (y[rxy] == 1):
+            if (y_hat[0] > 0.5) == (y[rxy] == 1):
                 correct[rxy] = 1
 
+            modelLoss += model.loss(x[rxy], y[rxy])[0]
+
         print("Epoch {}: {}/{}".format(e, correct.sum(), len(correct)))
+        print("      {}".format(modelLoss / X))
     
     # evaluate model
     correct = np.zeros(y.shape)
@@ -80,7 +127,7 @@ def training_loop(x, y):
     for rxy in range(len(x)):
         y_hat[rxy] = model.call(x[rxy])
 
-        if (y_hat[rxy] > 0.5) == (y[rxy] == 1):
+        if (y_hat[rxy][0] > 0.5) == (y[rxy] == 1):
             correct[rxy] = 1
 
     plot_confusion_matrix(y_hat > 0.5, y)
@@ -89,6 +136,9 @@ def training_loop(x, y):
 
     print(model.layer1.weights)
     print(model.layer2.weights)
+
+    plot_decision(model)
+    # plot_3d_plane(model)
 
 
 