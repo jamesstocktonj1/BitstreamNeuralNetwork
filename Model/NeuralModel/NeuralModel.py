2800c3d90523fd0063713241226588c59d48044a
js3g20 <js3g20@soton.ac.uk>
2023-02-15 10:14:06 +0000

    Add Simple Regularisation Model

--- Model/NeuralModel/NeuralModel.py
+++ Model/NeuralModel/NeuralModel.py
@@ -304,3 +304,47 @@ class HiddenModelRegular:
         z = self.layer1.call(x)
         z = self.layer2.call(z)
         return self.layer3.call(z)
+
+
+class SimpleModelRegular:
+
+    def __init__(self, R, L):
+        self.training_rate = R
+        self.regularisation = L
+
+        self.layer1 = NeuralLayer(2, 2)
+        self.layer2 = NeuralLayer(2, 1)
+
+        self.layer1.init_weights(0.1)
+        self.layer2.init_weights(0.1)
+
+    def grad(self, x, y):
+        z = self.layer1.call(x)
+        y_hat = self.layer2.call(z)
+        
+        dLoss = self.layer2.grad_loss(z, y)
+        dLayer2 = self.layer2.grad_layer(z, y_hat)
+        dWeight2 = self.layer2.grad_weight(z)
+
+        dWeight1 = self.layer1.grad_weight(x)
+
+        dW2 = dLoss * dWeight2
+        dW1 = np.dot(dLoss * dLayer2, dWeight1)
+
+        self.layer1.weights -= self.training_rate * dW1
+        self.layer1.weights -= self.regularisation * self.layer1.weights
+        self.layer1.weights = np.clip(self.layer1.weights, 0.0, 1.0)
+        self.layer2.weights -= self.training_rate * dW2
+        self.layer2.weights -= self.regularisation * self.layer2.weights
+        self.layer2.weights = np.clip(self.layer2.weights, 0.0, 1.0)
+
+        return dW2, dW1
+    
+    def loss(self, x, y):
+        y_hat = self.call(x)
+
+        return (y - y_hat) ** 2
+
+    def call(self, x):
+        z = self.layer1.call(x)
+        return self.layer2.call(z)
