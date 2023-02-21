307499c7ef93023db4c6c50c5ae3b884d9d7c788
js3g20 <js3g20@soton.ac.uk>
2023-02-21 15:36:40 +0000

    Add Increase Range

--- Stochastic/TestbedLFSR/main.py
+++ Stochastic/TestbedLFSR/main.py
@@ -22,6 +22,7 @@ def main():
     bs = []
 
     value = 184
+    expected = 0.72157
 
     for i in range(N):
         sr.shift()
@@ -29,18 +30,30 @@ def main():
 
     bs = np.array(bs)
 
-    split_count = 8
+    plt.figure()
+    mse = []
 
-    split_bs = split_bitstream(bs, split_count)
-    split_values = np.zeros(split_count)
+    for split_count in range(1, 16):
+        #split_count = 8
 
-    for i, b in enumerate(split_bs):
-        split_values[i] = b.sum() / b.size
-        print("{:.5f}, ".format(b.sum() / b.size), end="")
+        split_bs = split_bitstream(bs, split_count)
+        split_values = np.zeros(split_count)
 
-    expected = 0.72157
-    print("\nMean Square Error: {:.5f}".format(((expected - split_values) ** 2).sum()))
+        print("\nSplit Count: {}".format(split_count))
 
+        for i, b in enumerate(split_bs):
+            split_values[i] = b.sum() / b.size
+            print("{:.5f}, ".format(b.sum() / b.size), end="")
+
+        mse.append(((expected - split_values) ** 2).sum())
+
+        print("\nMean Square Error: {:.5f}".format(((expected - split_values) ** 2).sum()))
+
+    plt.xlabel("Number of Splits (255 / x)")
+    plt.ylabel("Mean Square Error")
+    plt.plot(list(range(1, 16)), mse)
+    plt.savefig("images/split_error.png")
+    plt.show()
 
     print("Bitstream Value: {}".format(bs.sum() / bs.size))
     print("Expected Value:  {}".format(value / N))
