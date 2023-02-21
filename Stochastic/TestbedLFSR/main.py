113891a79fbb13a7e43f01a92d08d487b07a97b3
js3g20 <js3g20@soton.ac.uk>
2023-02-21 14:23:26 +0000

    Add Bitstream Split Test

--- Stochastic/TestbedLFSR/main.py
+++ Stochastic/TestbedLFSR/main.py
@@ -1,22 +1,51 @@
-from LFSR import LFSR
+from LFSR import LFSR, LFSR16
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+N = 255
+
+
+def split_bitstream(bs, split):
+    N = bs.size // split
+
+    split_bs = np.zeros((split, N))
+
+    for i in range(split):
+        split_bs[i] = bs[N*i:N*(i+1)]
+
+    return split_bs
 
 
 def main():
     sr = LFSR(0b11010)
-    sr.put()
+    bs = []
 
-    val = 184
-    total = 0
+    value = 184
 
-    for i in range(255):
+    for i in range(N):
         sr.shift()
-        sr.put()
-        if sr.get() < val:
-            total += 1
+        bs.append((sr.get() < value) * 1)
 
-    print("Value: {:.04f}".format((val-1) / 255))
-    print("BS Value: {:.04f}".format(total / 255))
+    bs = np.array(bs)
+
+    split_count = 8
+
+    split_bs = split_bitstream(bs, split_count)
+    split_values = np.zeros(split_count)
+
+    for i, b in enumerate(split_bs):
+        split_values[i] = b.sum() / b.size
+        print("{:.5f}, ".format(b.sum() / b.size), end="")
+
+    expected = 0.72157
+    print("\nMean Square Error: {:.5f}".format(((expected - split_values) ** 2).sum()))
+
+
+    print("Bitstream Value: {}".format(bs.sum() / bs.size))
+    print("Expected Value:  {}".format(value / N))
+
+
 
 
 if __name__ == "__main__":
